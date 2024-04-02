7a823eef62ba27aaddde724097dab79ff1c2aa6f
Prashant patil <prashant@amazeinc.in>
2024-04-02 18:19:17 +0530

    w

--- src/app/revalidationtickets/revalidationtickets.component.ts
+++ src/app/revalidationtickets/revalidationtickets.component.ts
@@ -7,253 +7,278 @@ import { ExportToCsv } from 'export-to-csv-file';
 @Component({
   selector: 'app-revalidationtickets',
   templateUrl: './revalidationtickets.component.html',
-  styleUrls: ['./revalidationtickets.component.css']
+  styleUrls: ['./revalidationtickets.component.css'],
 })
 export class RevalidationticketsComponent implements OnInit {
-
   term: any;
   p: any = 1;
   count1: any = 10;
   stafflistCopy: any;
-  startdate:any;
-  enddate:any;
+  startdate: any;
+  enddate: any;
   count: any;
-  search:any;
-  issuefrom:any;
-  ticketList:any;
-  loader:any;
-  applicationName:any;
-  applicationNamelist:any;
-  companyName:any;
-  roleid:any;
-  ID:any;
-  CloseComments:any;
-  comment:any;
+  search: any;
+  issuefrom: any;
+  ticketList: any;
+  loader: any;
+  applicationName: any;
+  applicationNamelist: any;
+  companyName: any;
+  roleid: any;
+  ID: any;
+  CloseComments: any;
+  comment: any;
   companylist: any;
 
-  constructor(private AmazeSupportService:AmazeSupportService ) { }
+  constructor(private AmazeSupportService: AmazeSupportService) {}
   ngOnInit(): void {
-    this.companyName = "0"
-    this.applicationName="0"
-    this.issuefrom="0"
+    this.companyName = '0';
+    this.applicationName = '0';
+    this.issuefrom = '0';
     this.roleid = sessionStorage.getItem('roleid');
     // this.AmazeSupportService.GetSupportTickets().subscribe(data => {
     //   debugger
     //   this.ticketList = data.filter(x=>x.status=='accepted');
     //   this.stafflistCopy = this.ticketList
     // });
-    
+
     if (this.roleid == 3) {
-      this.AmazeSupportService.GetSupportTickets().subscribe(data => {
-        debugger
-        this.ticketList = data.filter(x => x.status == 'Reopen' );
-        this.stafflistCopy = this.ticketList
-
+      this.AmazeSupportService.GetSupportTickets().subscribe((data) => {
+        debugger;
+        this.ticketList = data.filter((x) => x.status == 'Reopened');
+        this.stafflistCopy = this.ticketList;
       });
-
     } else {
-      this.AmazeSupportService.GetSupportTickets().subscribe(data => {
-        debugger
-        this.ticketList = data.filter(x => x.status == 'Reopen');
-        this.stafflistCopy = this.ticketList
-
+      this.AmazeSupportService.GetSupportTickets().subscribe((data) => {
+        debugger;
+        this.ticketList = data.filter((x) => x.status == 'Reopened');
+        this.stafflistCopy = this.ticketList;
       });
     }
-    
+
     this.getCompanylist();
     this.getAppnamelist();
   }
+  public assign(ID: any) {
+    debugger;
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You want to assign it.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, assign it!',
+      cancelButtonText: 'No, keep it',
+    }).then((result) => {
+      if (result.value == true) {
+        var entity = {
+          id: ID,
+          Status: 'Assigned to DigiOffice',
+        };
 
- 
+        this.AmazeSupportService.UpdateAcceptStatusSupportTickets(
+          entity
+        ).subscribe((data) => {
+          debugger;
+
+          Swal.fire('Assigned Successfully');
+          location.reload();
+        });
+      }
+    });
+  }
+
   public getCompanylist() {
-
-    this.AmazeSupportService.GetSupportTickets().subscribe(data => {
-      debugger
-      let temp: any = data.filter(x => x.status == 'Reopen');
+    this.AmazeSupportService.GetSupportTickets().subscribe((data) => {
+      debugger;
+      let temp: any = data.filter((x) => x.status == 'Reopened');
 
       const key = 'companyname';
 
-      this.companylist = [...new Map(temp.map((item: { [x: string]: any; }) =>
-        [item[key], item])).values()];
+      this.companylist = [
+        ...new Map(
+          temp.map((item: { [x: string]: any }) => [item[key], item])
+        ).values(),
+      ];
 
       console.log(this.companylist);
-
     });
   }
   public getAppnamelist() {
-    this.AmazeSupportService.GetSupportTickets().subscribe(data => {
-      debugger
-      let temp: any = data.filter(x => x.status == 'Reopen');
+    this.AmazeSupportService.GetSupportTickets().subscribe((data) => {
+      debugger;
+      let temp: any = data.filter((x) => x.status == 'Reopened');
 
       const key = 'applicationName';
 
-      this.applicationNamelist = [...new Map(temp.map((item: { [x: string]: any; }) =>
-        [item[key], item])).values()];
+      this.applicationNamelist = [
+        ...new Map(
+          temp.map((item: { [x: string]: any }) => [item[key], item])
+        ).values(),
+      ];
 
       console.log(this.applicationNamelist);
-
     });
   }
-  
-
 
   public getaccept(even: any) {
-    this.ID = even
+    this.ID = even;
   }
 
   public close() {
-    debugger
-        var entity = {
-          'id': this.ID,
-          'Status': 'completed',
-          'CloseComments':this.CloseComments
-        }
-       
-        this.AmazeSupportService.UpdateCloseStatusSupportTickets(entity).subscribe(data => {
-          debugger
-          
-          Swal.fire('Closed Successfully')
-          location.reload();
-        })
-      }
+    debugger;
+    var entity = {
+      id: this.ID,
+      Status: 'completed',
+      CloseComments: this.CloseComments,
+    };
 
-      public GetCompanyName(evn: any) {
-        this.companyName = evn.target.value
-      }
-    
-      public GetFilteredCompanyName() {
-    
-        if (this.companyName == 0) {
-          this.AmazeSupportService.GetSupportTickets().subscribe(data => {
-            debugger
-            this.ticketList = data.filter(x => x.status == 'Reopen');
-    
-    
-          });
-        } else {
-          this.AmazeSupportService.GetSupportTickets().subscribe(data => {
-            debugger
-            this.ticketList = data.filter(x => x.status == 'Reopen' && x.companyname == this.companyName);
-          });
-        }
-      }
-    
-      public GetapplicationName(evn: any) {
-        this.applicationName = evn.target.value
-      }
-    
-      public GetFilteredapplicationName() {
-    
-        if (this.applicationName == 0) {
-          this.AmazeSupportService.GetSupportTickets().subscribe(data => {
-            debugger
-            this.ticketList = data.filter(x => x.status == 'Reopen');
-    
-    
-          });
-        } else {
-          this.AmazeSupportService.GetSupportTickets().subscribe(data => {
-            debugger
-            this.ticketList = data.filter(x => x.status == 'Reopen' && x.applicationName == this.applicationName);
-          });
-        }
-    
-      } 
-      view(desc: any) {
-        this.comment = desc;
-      }
+    this.AmazeSupportService.UpdateCloseStatusSupportTickets(entity).subscribe(
+      (data) => {
+        debugger;
 
-      attachmentlist:any;
-      image(id:any){
-        debugger
-        this.AmazeSupportService.GetSupportAttachment().subscribe(
-          data=>{
-            debugger
-           this.attachmentlist=data.filter(x=>x.ticketID==id);
-          
-          }
-        )
-        
+        Swal.fire('Closed Successfully');
+        location.reload();
       }
+    );
+  }
 
-      attachment:any;
-      getattchmentID(details:any){
-        this.attachment=details.attachment
-      }
-      public getenddate(event: any) {
-        debugger
-          this.AmazeSupportService.GetSupportTickets().subscribe(data => {
-            debugger
-            this.ticketList = data.filter(x => x.status == 'Reopen' && x.date>=this.startdate && x.date<=this.enddate);
-          });
-      }
-    
-      public GetFilteredissuefrom(event: any) {
-        this.issuefrom=event.target.value
-    
-        debugger
-          this.AmazeSupportService.GetSupportTickets().subscribe(data => {
-            debugger
-            this.ticketList = data.filter(x =>x.issuefrom== this.issuefrom && x.status == 'Reopen');
-          });
-      }
+  public GetCompanyName(evn: any) {
+    this.companyName = evn.target.value;
+  }
 
-       //Code for Export to excel//
-       undefined: any;
-       sequenceNumber1: any
-       attendancelist: any
-       public exportexcel() {
-         debugger
-         var ExportData = [];
-         this.sequenceNumber1 = 0;
-         this.undefined = 'NA'
-         for (let i = 0; i < this.ticketList.length; i++) {
-           debugger;
-           this.sequenceNumber1 = i + 1;
-           let singleData = {
-             IssueFrom: String,
-             Id: String,
-             CompanyName: String,
-             ApplicationName: String,
-             EmployeeID: String,
-             Role: String,
-             Date: String,
-             Time: String,
-             TypeOfApplicationIssue: String,
-             Priority: String,
-           }
-          
-           singleData.IssueFrom = this.ticketList[i].issuefrom;
-           singleData.Id = this.ticketList[i].id;
-           singleData.CompanyName = this.ticketList[i].companyname;
-           singleData.ApplicationName = this.ticketList[i].applicationName;
-           singleData.EmployeeID = this.ticketList[i].staffID
-           singleData.Role = this.ticketList[i].role;
-           singleData.Date = this.ticketList[i].date;
-           singleData.Time = this.ticketList[i].time;
-           singleData.TypeOfApplicationIssue = this.ticketList[i].typeOfApplicationIssues
-           singleData.Priority = this.ticketList[i].priority;
-           singleData.Date = this.ticketList[i].date;
-     
-     
-           ExportData.push(singleData);
-           debugger
-         }
-         const Export_to_excel_options = {
-           fieldSeparator: ',',
-           quoteStrings: '"',
-           decimalSeparator: '.',
-           showLabels: true,
-           showTitle: true,
-           title: 'Accept REPORT.xlsx',
-           useTextFile: false,
-           useBom: true,
-           useKeysAsHeaders: true,
-           filename: 'EMPLOYEE ATTEDANCE REPORT'
-         };
-         const csvExporter = new ExportToCsv(Export_to_excel_options);
-         debugger
-         csvExporter.generateCsv(ExportData);
-     
-       }
+  public GetFilteredCompanyName() {
+    if (this.companyName == 0) {
+      this.AmazeSupportService.GetSupportTickets().subscribe((data) => {
+        debugger;
+        this.ticketList = data.filter((x) => x.status == 'Reopened');
+      });
+    } else {
+      this.AmazeSupportService.GetSupportTickets().subscribe((data) => {
+        debugger;
+        this.ticketList = data.filter(
+          (x) => x.status == 'Reopened' && x.companyname == this.companyName
+        );
+      });
+    }
+  }
+
+  public GetapplicationName(evn: any) {
+    this.applicationName = evn.target.value;
+  }
+
+  public GetFilteredapplicationName() {
+    if (this.applicationName == 0) {
+      this.AmazeSupportService.GetSupportTickets().subscribe((data) => {
+        debugger;
+        this.ticketList = data.filter((x) => x.status == 'Reopened');
+      });
+    } else {
+      this.AmazeSupportService.GetSupportTickets().subscribe((data) => {
+        debugger;
+        this.ticketList = data.filter(
+          (x) =>
+            x.status == 'Reopened' && x.applicationName == this.applicationName
+        );
+      });
+    }
+  }
+  view(desc: any) {
+    this.comment = desc;
+  }
+
+  attachmentlist: any;
+  image(id: any) {
+    debugger;
+    this.AmazeSupportService.GetSupportAttachment().subscribe((data) => {
+      debugger;
+      this.attachmentlist = data.filter((x) => x.ticketID == id);
+    });
+  }
+
+  attachment: any;
+  getattchmentID(details: any) {
+    this.attachment = details.attachment;
+  }
+  public getenddate(event: any) {
+    debugger;
+    this.AmazeSupportService.GetSupportTickets().subscribe((data) => {
+      debugger;
+      this.ticketList = data.filter(
+        (x) =>
+          x.status == 'Reopened' &&
+          x.date >= this.startdate &&
+          x.date <= this.enddate
+      );
+    });
+  }
+
+  public GetFilteredissuefrom(event: any) {
+    this.issuefrom = event.target.value;
+
+    debugger;
+    this.AmazeSupportService.GetSupportTickets().subscribe((data) => {
+      debugger;
+      this.ticketList = data.filter(
+        (x) => x.issuefrom == this.issuefrom && x.status == 'Reopened'
+      );
+    });
+  }
+
+  //Code for Export to excel//
+  undefined: any;
+  sequenceNumber1: any;
+  attendancelist: any;
+  public exportexcel() {
+    debugger;
+    var ExportData = [];
+    this.sequenceNumber1 = 0;
+    this.undefined = 'NA';
+    for (let i = 0; i < this.ticketList.length; i++) {
+      debugger;
+      this.sequenceNumber1 = i + 1;
+      let singleData = {
+        IssueFrom: String,
+        Id: String,
+        CompanyName: String,
+        ApplicationName: String,
+        EmployeeID: String,
+        Role: String,
+        Date: String,
+        Time: String,
+        TypeOfApplicationIssue: String,
+        Priority: String,
+      };
+
+      singleData.IssueFrom = this.ticketList[i].issuefrom;
+      singleData.Id = this.ticketList[i].id;
+      singleData.CompanyName = this.ticketList[i].companyname;
+      singleData.ApplicationName = this.ticketList[i].applicationName;
+      singleData.EmployeeID = this.ticketList[i].staffID;
+      singleData.Role = this.ticketList[i].role;
+      singleData.Date = this.ticketList[i].date;
+      singleData.Time = this.ticketList[i].time;
+      singleData.TypeOfApplicationIssue =
+        this.ticketList[i].typeOfApplicationIssues;
+      singleData.Priority = this.ticketList[i].priority;
+      singleData.Date = this.ticketList[i].date;
+
+      ExportData.push(singleData);
+      debugger;
+    }
+    const Export_to_excel_options = {
+      fieldSeparator: ',',
+      quoteStrings: '"',
+      decimalSeparator: '.',
+      showLabels: true,
+      showTitle: true,
+      title: 'Accept REPORT.xlsx',
+      useTextFile: false,
+      useBom: true,
+      useKeysAsHeaders: true,
+      filename: 'EMPLOYEE ATTEDANCE REPORT',
+    };
+    const csvExporter = new ExportToCsv(Export_to_excel_options);
+    debugger;
+    csvExporter.generateCsv(ExportData);
+  }
 }
